919a73857d640250305514050cbf1dd79f2bc16c
Justin Bruinsma <justinabruinsma@gmail.com>
2025-04-26 14:05:30 -0400

    Updated BinarySearch.java to add a static main method for testing, and to return the index of the search target rather than a boolean.

--- src/SearchingAndSorting/Searching/BinarySearch.java
+++ src/SearchingAndSorting/Searching/BinarySearch.java
@@ -13,21 +13,29 @@ public class BinarySearch {
 
     public int[] getArr() { return this.arr; }
 
-    public boolean search(int[] arr, int start, int end, int key) {
+    public Object search(){
+        return search(arr, 0, arr.length - 1, 99);
+    }
 
-        if (start > end) return false;
+    public Object search(int[] arr, int start, int end, int key) {
+
+        if (start > end) return null;
 
         int mid = start + (end - start) / 2;
 
-        if (arr[mid] == key){ return true; }
+        if (arr[mid] == key){ return mid; }
         else if (arr[mid] > key){ return search(arr, start, mid- 1, key); }
         else { return search(arr, mid + 1, end, key); }
 
     }
 
     public static void main(String[] args){
+        System.out.println("Binary Search Test: \n");
+        System.out.println("Searching for 99 in the array [45, 77, 89, 90, 94, 99, 100]");
         BinarySearch bs = new BinarySearch(new int[]{45, 77, 89, 90, 94, 99, 100});
-        System.out.println(bs.search(bs.getArr(), 0, bs.getArr().length, 99));
+        System.out.println("99 is at Index: " + bs.search() + "\n");
+        System.out.println("Searching for 105 in the array [45, 77, 89, 90, 94, 99, 100]");
+        System.out.println("105 is at Index: " + bs.search(new int[]{45, 77, 89, 90, 94, 99, 100}, 0, 6, 105));
     }
 
 }
