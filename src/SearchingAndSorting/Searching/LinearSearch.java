5291ffbfd14425fb2d64e08e0f0d74a1594a83da
Justin Bruinsma <justinabruinsma@gmail.com>
2025-04-26 14:42:22 -0400

    Updated LinearSearch.java to incorporate Generic Programming types for flexibility.

--- src/SearchingAndSorting/Searching/LinearSearch.java
+++ src/SearchingAndSorting/Searching/LinearSearch.java
@@ -1,20 +1,38 @@
 package SearchingAndSorting.Searching;
 
-public class LinearSearch {
-    private int[] arr;
+public class LinearSearch<T> {
+    private T[] arr;
 
-    public LinearSearch(int[] anArray) {
-        arr = anArray;
+    public LinearSearch(T[] array) {
+        arr = array;
     }
 
-    public void setArr(int[] arr){
+    public void setArr(T[] arr){
         this.arr = arr;
     }
 
-    public int search(int v) {
+    public int search(T target) {
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == v) return i;
+            if (arr[i] == target) return i;
         }
         return -1;
+    }
+
+    public boolean contains(T target) {
+        return search(target) != -1;
+    }
+
+    public static void main(String[] args) {
+        Integer[] vals = {45, 77, 89, 90, 94, 99, 100};
+        LinearSearch<Integer> integerLinearSearch = new LinearSearch<>(vals);
+        System.out.println("Searching for 99 in the array [45, 77, 89, 90, 94, 99, 100]");
+        System.out.println("90 is at Index: " + integerLinearSearch.search(90));
+        System.out.println("Contains 5? " + integerLinearSearch.contains(5) + "\n");
+
+        String[] names = {"Aiden", "Bella", "Carter", "Delilah", "Eliana", "Finn", "Hazel", "Leo", "Maya", "Silas"};
+        LinearSearch<String> nameLinearSearch = new LinearSearch<>(names);
+        System.out.println("Searching for Carter in the array [Aiden, Bella, Carter, Delilah, Eliana, Finn, Hazel, Leo, Maya, Silas]");
+        System.out.println("Carter is at Index: " + nameLinearSearch.search("Carter"));
+        System.out.println("Contains Maya? " + nameLinearSearch.contains("Maya") + "\n");
     }
 }
