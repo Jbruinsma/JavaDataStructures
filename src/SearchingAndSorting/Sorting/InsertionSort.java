9cd9c8db2ff4cd68f37aa5c05e7b0694caa3a12f
Justin Bruinsma <justinabruinsma@gmail.com>
2025-04-27 18:09:40 -0400

    Updated InsertionSort.java.java to incorporate Generic Programming types for flexibility.

--- src/SearchingAndSorting/Sorting/InsertionSort.java
+++ src/SearchingAndSorting/Sorting/InsertionSort.java
@@ -1,44 +1,46 @@
 package SearchingAndSorting.Sorting;
 
-public class InsertionSort {
-    private int[] arr;
+import java.util.Arrays;
 
-    public InsertionSort(int[] arrArray) {
+public class InsertionSort<T extends Comparable<T>> {
+    private T[] arr;
+
+    public InsertionSort(T[] arrArray) {
         arr = arrArray;
     }
 
-    public void setArr(int[] arr){
+    public void setArr(T[] arr){
         this.arr = arr;
     }
 
     public void insertionSort() {
         for (int i = 1; i < arr.length; i++) {
-            int nextLoc = arr[i];
+            T nextElement = arr[i];
             int j = i;
-            while (j > 0 && arr[j - 1] > nextLoc) {
+            while (j > 0 && arr[j - 1].compareTo(nextElement) > 0) {
                 arr[j] = arr[j - 1];
                 j--;
             }
-            arr[j] = nextLoc;
+            arr[j] = nextElement;
         }
     }
 
-    public void printArray() {
-        for (int num : arr) {
-            System.out.print(num + " ");
-        }
-        System.out.println();
+    public T[] getArr() {
+        return this.arr;
     }
 
     public static void main(String[] args) {
-        int[] arr = {5, 2, 9, 1, 5, 6};
-        InsertionSort insertionSort = new InsertionSort(arr);
-        System.out.println("Original array:");
-        insertionSort.printArray();
+        Integer[] vals = {5, 2, 9, 1, 5, 6};
+        System.out.println("Insertion Sort: \n");
+        InsertionSort<Integer> integerInsertionSort = new InsertionSort<>(vals);
+        System.out.println("Before Sorting: " + Arrays.toString(integerInsertionSort.getArr()) + "\n");
+        integerInsertionSort.insertionSort();
+        System.out.println("After Sorting: " + Arrays.toString(integerInsertionSort.getArr()) + "\n");
 
-        insertionSort.insertionSort();
-
-        System.out.println("Sorted array:");
-        insertionSort.printArray();
+        String[] names = {"Hazel", "Maya", "Finn", "Carter", "Silas", "Eliana", "Leo", "Bella", "Aiden", "Delilah"};
+        InsertionSort<String> nameInsertionSort = new InsertionSort<>(names);
+        System.out.println("Before Sorting: " + Arrays.toString(nameInsertionSort.getArr()) + "\n");
+        nameInsertionSort.insertionSort();
+        System.out.println("After Sorting: " + Arrays.toString(nameInsertionSort.getArr()) + "\n");
     }
 }
